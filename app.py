2234c32a50aeecee67cd2e49182266a091520e16
Thiago Hardt <thiago.hardts@gmail.com>
2020-11-03 16:58:31 +0000

    Added script, style and base.html

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import (Flask, flash, render_template,
-                   redirect, request, session, url_for)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectId import ObjectId
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
